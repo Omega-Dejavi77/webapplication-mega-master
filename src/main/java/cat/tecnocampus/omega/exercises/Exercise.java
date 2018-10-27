20252d4ba5c3509d14fcbfc4799ce1ba8a0f3f4d
DESKTOP-QJ9CNA9\Erik Espuñes Juberó <erik.espunyes7@outlook.com>
2018-10-28 01:48:16 +0200

    Exercise Abstract class implemented

--- src/main/java/cat/tecnocampus/omega/exercises/Exercise.java
+++ src/main/java/cat/tecnocampus/omega/exercises/Exercise.java
@@ -1,4 +1,55 @@
 package cat.tecnocampus.omega.exercises;
 
-public class Exercise {
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public abstract class Exercise {
+    @NotNull
+    protected String exercise_ID;
+
+    @NotNull
+    @Size(max = 1024,message = "Description too long")
+    protected String description;
+    @NotNull
+    protected boolean enable;
+    @NotNull
+    protected int difficulty;
+    protected int experience_points;
+    public Exercise(String description,int difficulty) {
+        this.description = description;
+        this.difficulty=difficulty;
+        //ID
+    }
+
+    public String getExercise_ID() {
+        return exercise_ID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public int getExperience_points() {
+        return experience_points;
+    }
+
+    public void setExperience_points(int experience_points) {
+        this.experience_points = experience_points;
+    }
 }
