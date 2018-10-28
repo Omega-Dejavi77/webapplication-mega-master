5704b3a661d144eb9d8933df05752ae28d3d6ec2
DESKTOP-QJ9CNA9\Erik Espuñes Juberó <erik.espunyes7@outlook.com>
2018-10-28 02:44:13 +0200

    Added ExerciseDAO but it lacks of information 'cause i need to see the database

---
+++ src/main/java/cat/tecnocampus/omega/persistance/ExerciseDAO.java
@@ -0,0 +1,23 @@
+package cat.tecnocampus.omega.persistance;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExerciseDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    private final String INSERT_TEST_EXERCISE="";
+    private final String INSERT_FILL_THE_GAP_EXERCISE="";
+    private final String INSERT_QUESTION="";
+    private final String INSERT_SOLUTION="";
+
+    private final String DELETE_TEST_EXERCISE="UPDATE Text SET enable = 0 WHERE test_ID = ?";
+    private final String DELETE_FILL_THE_GAP_EXERCISE="";
+    private final String DELETE_QUESTION="";
+    private final String DELETE_SOLUTION="";
+
+    private final String SELECT_TEST_BY_TUTORIAL="";
+    private final String SELECT_FILL_THE_GAP_BY_TUTORIAL="";
+    private final String SELECT_QUESTION_BY_EXERCISE="";
+    private final String SELECT_SOLUTION_BY_QUESTION="";
+}
