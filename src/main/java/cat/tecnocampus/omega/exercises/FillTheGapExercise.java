cba0fff958b36c193bfa7d1b396d9ee4c35794a9
DESKTOP-QJ9CNA9\Erik Espuñes Juberó <erik.espunyes7@outlook.com>
2018-10-29 13:16:57 +0100

    AOP don't belong here

--- src/main/java/cat/tecnocampus/omega/exercises/FillTheGapExercise.java
+++ src/main/java/cat/tecnocampus/omega/exercises/FillTheGapExercise.java
@@ -1,7 +1,5 @@
 package cat.tecnocampus.omega.exercises;
 
-import java.util.List;
-
 public class FillTheGapExercise extends Exercise {
     public FillTheGapExercise(String exercise_ID,String description, int difficulty) {
         super(exercise_ID,description, difficulty);
