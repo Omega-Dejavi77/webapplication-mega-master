b3bf9ecebf9bb22846c3a444b19304b65c0a0a72
Jose Oviedo <joseaoviedof>
2018-11-02 12:07:38 +0100

    Fix DataBase

--- src/main/resources/Exercice.sql
+++ src/main/resources/Exercice.sql
@@ -5,5 +5,6 @@ create table Exercises(
   enable integer constraint not null,
   dificullty integer constraint not null,
   experience_points integer,
+  son_TYPE varchar (36),
   post_ID varchar (36) constraint foreign key references Post(post_ID)
 );
