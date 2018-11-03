63adeef7e9625867f30393d0acb5cfba4d82c399
Jose Oviedo <joseaoviedof>
2018-11-03 15:49:35 +0100

    Delete Wrong tables

--- src/main/resources/Question.sql
+++
@@ -1,7 +0,0 @@
-drop table IF EXISTS  Questions;
-CREATE table Questions (
-    question_ID varchar (36) primary key,
-    text varchar (36) not null,
-    exercise_ID varchar (36) foreign key references Exercises(exercise_ID),
-    solution_ID varchar (36) foreign key references Solutions(solution_ID)
-);
