ea8f9728446a1860a99289220637954c98b96d40
Jose Oviedo <joseaoviedof>
2018-11-03 21:37:56 +0100

    Converted

--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -1,52 +1,58 @@
 drop table IF EXISTS Posts;
 CREATE TABLE Posts (
-    post_ID varchar(36) PRIMARY KEY,
-    Name varchar(64)  NOT NULL,
+    post_id varchar(36) PRIMARY KEY,
+    name varchar(64)  NOT NULL,
     description varchar (1024) not null,
     creationDay DATE not null,
     likes INTEGER,
     enable integer not NULL,
-    son_TYPE varchar (36),
-    startingDate date,
+    son_type varchar (36),
+    starting_date date,
     deadline date,
-    post_ID_ref varchar (36) foreign key references Posts(post_ID)
+    post_id_ref varchar (36),
+    foreign key (post_id_ref) references Posts(post_id)
 );
 
 drop table if exists Exercises;
 create table Exercises(
-  exercise_ID varchar (36)primary key,
+  exercise_id varchar (36)primary key,
   description varchar (1024) not null,
   enable integer not null,
   dificullty integer not null,
   experience_points integer,
-  son_TYPE varchar (36),
-  post_ID varchar (36)  foreign key references Post(post_ID)
+  son_type varchar (36),
+  post_id varchar (36),
+  foreign key (post_id) references Post(post_ID)
 );
 drop table IF EXISTS  Questions;
 CREATE table Questions (
-    question_ID varchar (36) primary key,
+    question_id varchar (36) primary key,
     texts varchar (36) not null,
-    exercise_ID varchar (36) foreign key references Exercises(exercise_ID),
-    solution_ID varchar (36) foreign key references Solutions(solution_ID)
+    exercise_id varchar (36),
+    solution_id varchar (36),
+  foreign key (exercise_id) references Exercises(exercise_id),
+  foreign key (solution_id) references Solutions(solution_id)
 );
 drop table if EXISTS Solutions ;
 create table Solutions(
-    solution_ID varchar (36) primary key,
-    order number,
-    texts varchar (36 not null ,
+    solution_id varchar (36) primary key,
+    position number,
+    texts varchar (36) not null ,
     correct BIT,
     enable integer not null,
-    question_ID varchar (36) foreign key references Questions(question_ID)
+    question_id varchar (36),
+  foreign key (question_id) references Questions(question_id)
 );
 drop table if EXISTS Comments;
 create table Comments(
     comment_id varchar (36) primary key,
     comment varchar (2048) not null,
-    creation_day DATE collation_schema not null,
+    creation_day DATE not null,
     likes integer,
     best BIT,
     enable BIT  not NULL ,
-    post_ID varchar (36) foreign key references Posts(post_ID)
+    post_id varchar (36),
+    foreign key (post_id) references Posts(post_id)
 );
 
 drop table if EXISTS Users;
@@ -64,7 +70,8 @@ create TABLE Users (
 drop table if EXISTS Roles;
 create table Roles(
     role varchar (32) primary key ,
-    user_name varchar (64) foreign key references Users(username)
+    user_name varchar (64),
+    foreign key (user_name) references Users(username)
 );
 drop table if exists Category;
 create table Category(
@@ -72,14 +79,18 @@ create table Category(
 );
 drop table if exists messages;
 create table messages(
-    message_ID varchar (36)primary key,
+    message_id varchar (36)primary key,
     content varchar (1024) not null,
-    user_sender varchar (64) foreign key references Users(username),
-    user_receiver varchar (64) foreign key references Users(username)
+    user_sender varchar (64),
+    user_receiver varchar (64),
+  foreign key (user_sender) references Users(username),
+  foreign key (user_reciever) references Users(username)
 );
 drop table if exists Submissions;
 create table Submissions(
     mark float not null,
-    username varchar (36) foreign key references Users(username),
-    exercise varchar (36) foreign key references Exercises(exercise_ID)
+    username varchar (36),
+    exercise varchar (36),
+  foreign key (username) references Users(username),
+  foreign key (exercise) references Exercises(exercise_ID)
 );
