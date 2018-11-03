c1d8e45482d3a79a267a93e7df981f363a274ff4
Jose Oviedo <joseaoviedof>
2018-11-03 11:46:20 +0100

    Firs to First

--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -53,7 +53,7 @@ drop table if EXISTS Users;
 create TABLE Users (
     username varchar (64) primary key,
     password varchar (32) not null,
-    firs_name varchar (128) not null,
+    first_name varchar (128) not null,
     last_name varchar (128) not null,
     email varchar (256) not null,
     birthday DATE,
