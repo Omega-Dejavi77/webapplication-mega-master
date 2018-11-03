33fd07c59a8dd6995721c65fe800b889bd77efec
Jose Oviedo <joseaoviedof>
2018-11-03 09:54:01 +0100

    Realm Unificated (Schema created and done)

--- src/main/resources/Submission.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists Submissions;
-create table Submissions(
-    mark float constraint not null,
-    username varchar (36) foreign key references Users(username),
-    exercise varchar (36) foreign key references Exercises(exercise_ID)
-);
