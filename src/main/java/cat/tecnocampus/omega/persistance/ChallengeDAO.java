1d50bca1874396f584a8646298cc713130dc1747
Jose Oviedo <joseaoviedof>
2018-11-03 13:12:02 +0100

    Challenge finished and Chalenge DAO

---
+++ src/main/java/cat/tecnocampus/omega/persistance/ChallengeDAO.java
@@ -0,0 +1,29 @@
+package cat.tecnocampus.omega.persistance;
+
+import cat.tecnocampus.omega.post.Challenge;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class ChallengeDAO {
+
+    private JdbcTemplate jdbcTemplate;
+    private final String FIND_ALL = "select * from posts";
+    private final String INSERT = "insert into posts (post_ID, title, description, creationDay, likes, enable, son_TYPE) values(?, ?, ?, ?, ?,?,'Challenge')";
+
+    public ChallengeDAO(JdbcTemplate jdbcTemplate) {
+
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public List<Challenge> findAll() {
+        return jdbcTemplate.query(FIND_ALL,new BeanPropertyRowMapper<>(Challenge.class));
+    }
+
+    public int insertTutorial(Challenge challenge) {
+        return jdbcTemplate.update(INSERT,challenge.getPostID(),challenge.getTitle(),challenge.getDescription(),challenge.getCreationDay(), challenge.getLikes(),challenge.isEnable());
+    }
+
+}
