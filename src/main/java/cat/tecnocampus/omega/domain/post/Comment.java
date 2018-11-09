d96de18ded63b8b4a3f10a91e1deb5bbfe8eff0d
DESKTOP-D5UBNR1\FoxisXIII <sofi.b.a97@gmail.com>
2018-11-09 23:32:01 +0100

    Discussion and Comment Class fully apparently implemented.

--- src/main/java/cat/tecnocampus/omega/domain/post/Comment.java
+++ src/main/java/cat/tecnocampus/omega/domain/post/Comment.java
@@ -1,11 +1,85 @@
 package cat.tecnocampus.omega.domain.post;
 
 import cat.tecnocampus.omega.domain.*;
+import java.util.Date;
+import java.util.UUID;
 
 public class Comment {
 
     private Discussion discussion;
     private User user;
-    private
+    private String commentID;
+    private String comment;
+    private Date creationDay;
+    private int likes;
+    private boolean enable;
+    private boolean bestComment;
 
+
+    public Comment (String ID, String comment, User user, Discussion discussion){
+        commentID=ID;
+        this.comment=comment;
+        this.user=user;
+        this.discussion=discussion;
+        creationDay=new Date();
+        likes=0;
+        enable=true;
+        bestComment=false;
+    }
+
+    public Comment (String comment, User user, Discussion discussion){
+        commentID=UUID.randomUUID().toString();
+        this.comment=comment;
+        this.user=user;
+        this.discussion=discussion;
+        creationDay=new Date();
+        likes=0;
+        enable=true;
+        bestComment=false;
+    }
+
+    public void editComment (String newComment){
+        comment=newComment;
+    }
+
+    public void addLike(){
+        likes++;
+    }
+
+    void setBestComment(boolean stat){
+        bestComment=stat;
+        discussion.setHasBestComment(stat,this);
+    }
+
+    public Discussion getDiscussion() {
+        return discussion;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getCommentID() {
+        return commentID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getCreationDay() {
+        return creationDay;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public boolean isBestComment() {
+        return bestComment;
+    }
 }
--- src/main/java/cat/tecnocampus/omega/domain/post/Discussion.java
+++ src/main/java/cat/tecnocampus/omega/domain/post/Discussion.java
@@ -2,22 +2,38 @@ package cat.tecnocampus.omega.domain.post;
 
 import java.util.ArrayList;
 import java.util.List;
+import cat.tecnocampus.omega.domain.*;
 
 public class Discussion extends APost {
 
+    private boolean hasBestComment;
+    private Comment bestComment;
+    private User user;
 
     private List<Comment> comments= new ArrayList<Comment>();
 
-    public Discussion(String postID, String description, String title) {
+    public Discussion(String postID, String description, String title,User user) {
         super(postID, description, title);
+        hasBestComment=false;
+        this.user=user;
     }
 
-    public Discussion(String body, String title) {
+    public Discussion(String body, String title,User user) {
         super(body, title);
+        hasBestComment=false;
+        this.user=user;
     }
 
     public void addComment(Comment comment){
         comments.add(comment);
     }
 
+    void setHasBestComment(boolean stat,Comment comment){
+        hasBestComment=stat;
+        bestComment=comment;
+    }
+
+    User getUser(){
+        return user;
+    }
 }
