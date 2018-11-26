5e1bbed1cae796eb18dcf4d77b3b5c217523b0a0
DESKTOP-D5UBNR1\FoxisXIII <sofi.b.a97@gmail.com>
2018-11-26 10:15:46 +0100

    Name of the library changed, still trying to know how to use it.

--- src/main/java/cat/tecnocampus/omega/domain/exercises/FillTheGapExercise.java
+++ src/main/java/cat/tecnocampus/omega/domain/exercises/FillTheGapExercise.java
@@ -1,5 +1,7 @@
 package cat.tecnocampus.omega.domain.exercises;
 
+
+
 public class FillTheGapExercise extends Exercise {
     public FillTheGapExercise(String exercise_ID, String description, int difficulty, boolean drag) {
         super(exercise_ID, description, difficulty);
