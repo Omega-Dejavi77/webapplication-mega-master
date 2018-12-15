7ce692b2c0811c2d3e89602053d722ed94eef08e
Dejavi77 <javierdelaserna7@gmail.com>
2018-12-15 12:25:52 +0100

    some bootstrapp added

--- src/main/java/cat/tecnocampus/omega/security/BaseSecurityConfiguration.java
+++ src/main/java/cat/tecnocampus/omega/security/BaseSecurityConfiguration.java
@@ -10,10 +10,9 @@ public class BaseSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
                 .antMatchers("/createUser").permitAll()
                 .antMatchers("/allUsers").hasRole("ADMIN")
-                .antMatchers("/profile/users/*").permitAll()
+                //.antMatchers("/profile/users/*").permitAll()
                 .antMatchers("/user").authenticated()
                 .antMatchers("/users/{username}").authenticated()
 
@@ -26,25 +25,31 @@ public class BaseSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/challenge/**").authenticated()
                 .antMatchers("/tutorial/**").permitAll()
                 .antMatchers("/css/**").permitAll()
+                .antMatchers("/**").permitAll()
+
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
 //                .defaultSuccessUrl("/users/{username}")
+
                 .and()
                 .httpBasic()
                 .and()
+
                 .rememberMe()
                 .tokenValiditySeconds(241920)
+
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/Errors.html")
+                .logoutSuccessUrl("/")
                 .permitAll();
+
         http
                 .csrf().disable()
                 .headers()
-                .frameOptions().disable()
-                .and()
-                .cors();
+                .frameOptions().disable();
+                /*.and()
+                .cors();*/
     }
 }
